62f8756807e6dafdfb645029bb76cb4161a27e78
vasileios.drosatos <vasileios.drosatos@workable.com>
2021-08-20 00:48:35 +0300

    Fix tests

--- packages/pwa/test/testUtils.ts
+++ packages/pwa/test/testUtils.ts
@@ -16,3 +16,17 @@ export { customRender as render }
 jest.mock('react-i18next', () => ({
   useTranslation: () => ({t: key => key})
 }));
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+})
