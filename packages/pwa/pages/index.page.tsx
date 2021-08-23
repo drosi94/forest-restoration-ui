2ea61005a0c36c523e2111c41e966ed5ca2d80db
vasileios.drosatos <vasileios.drosatos@workable.com>
2021-08-23 04:22:11 +0300

    Update index.page.tsx

--- packages/pwa/pages/index.page.tsx
+++ packages/pwa/pages/index.page.tsx
@@ -5,10 +5,7 @@ import Router from 'next/router'
 export default function Home() {
   useEffect(() => {
     const { pathname } = Router
-    console.log('pathname', pathname)
-    console.log('process.env.MAINTENANCE_MODE ', process.env)
     if (process.env.NEXT_PUBLIC_MAINTENANCE_MODE == 'true' && pathname == '/') {
-      console.log('edw')
       Router.push('/maintenance')
     }
   })
