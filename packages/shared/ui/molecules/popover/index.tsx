41d953a48804a956e965d113a0dd543eba35aecf
vasileios.drosatos <vasileios.drosatos@workable.com>
2021-08-23 19:00:35 +0300

    Add Popover component

---
+++ packages/shared/ui/molecules/popover/index.tsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react'
+import tw from 'twin.macro'
+import { usePopper } from 'react-popper'
+import type { Placement } from '@popperjs/core'
+import { Popover as BasePopover } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+import { Button, Typography } from '../../atoms'
+
+export type PopoverProps = {
+  /**
+   * The label of the button
+   */
+  label: string
+  buttonComponent?: any
+  containerComponent?: any
+  panelComponent?: any
+  placement?: Placement
+  children: React.ReactNode
+}
+
+export const Popover: React.FC<PopoverProps> = ({
+  children,
+  label,
+  buttonComponent = Button,
+  containerComponent,
+  panelComponent,
+  placement = 'bottom-start',
+}) => {
+  const [referenceElement, setReferenceElement] = useState()
+  const [popperElement, setPopperElement] = useState()
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement,
+    modifiers: [
+      {
+        name: 'preventOverflow',
+        options: {
+          boundary: 'clippingParents',
+        },
+      },
+      {
+        name: 'flip',
+        options: {
+          allowedAutoPlacements: ['bottom-end'],
+          fallbackPlacements: ['bottom-end', 'top-start'],
+          altBoundary: true,
+        },
+      },
+      {
+        name: 'offset',
+        options: {
+          offset: [0, 4],
+        },
+      },
+    ],
+  })
+  return (
+    <BasePopover as={containerComponent} css={[tw`relative`]}>
+      {({ open }) => (
+        <>
+          <BasePopover.Button
+            as={buttonComponent}
+            ref={setReferenceElement}
+            css={[
+              tw`
+                 px-3 py-2  inline-flex items-center hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`,
+              !open && tw`text-opacity-90`,
+            ]}
+          >
+            <Typography>{label}</Typography>
+            <ChevronDownIcon
+              css={[
+                tw`
+              ml-2 h-5 w-5 text-textPrimary group-hover:text-opacity-80 transition ease-in-out duration-150`,
+                !open && tw`text-opacity-70`,
+              ]}
+              aria-hidden="true"
+            />
+          </BasePopover.Button>
+          <BasePopover.Overlay />
+          {/* @ts-ignore */}
+          <BasePopover.Panel
+            as={panelComponent}
+            ref={setPopperElement}
+            style={styles.popper}
+            {...attributes.popper}
+          >
+            <div tw="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+              {children}
+            </div>
+          </BasePopover.Panel>
+        </>
+      )}
+    </BasePopover>
+  )
+}
---
+++ packages/ui/stories/ui/Popover.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
+import tw from 'twin.macro'
+
+import { Popover, PopoverProps } from '@forest-restoration/shared'
+
+export default {
+  title: 'Shared/UI/Popover',
+  component: Popover,
+} as Meta
+
+const Template: Story<PopoverProps> = (args) => (
+  <div tw="flex items-center justify-center h-screen">
+    <Popover {...args} />
+  </div>
+)
+
+export const Primary = Template.bind({})
+Primary.args = {
+  label: 'Popover',
+  children: (
+    <div tw="relative bg-bgSecondary text-white p-4 w-full">
+      <span>Here I am</span>
+    </div>
+  ),
+}
+
+export const Top = Template.bind({})
+Top.args = {
+  ...Primary.args,
+  placement: 'top-start',
+}
