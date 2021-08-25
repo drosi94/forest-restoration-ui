0d1e53024506e8f915ef82ac2274f4c521298b11
vasileios.drosatos <vasileios.drosatos@workable.com>
2021-08-25 11:58:05 +0300

    Add more variations

--- packages/ui/stories/ui/form/Autocomplete.stories.tsx
+++ packages/ui/stories/ui/form/Autocomplete.stories.tsx
@@ -10,12 +10,11 @@ export default {
 
 const Template: Story<AutocompleteProps> = (args) => {
   const options = [
-    { id: 1, name: 'Egw' },
-    { id: 2, name: 'Esy' },
-    { id: 3, name: 'Aytos' },
-    { id: 4, name: 'Emeis' },
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Joe' },
+    { id: 3, name: 'Mary' },
+    { id: 4, name: 'Hellen' },
   ]
-  const [value, setValue] = React.useState(options[0])
   return <Autocomplete options={options} optionLabelItem="name" {...args} />
 }
 
@@ -24,4 +23,22 @@ Primary.args = {
   id: 'test',
   name: 'test',
   label: 'Label',
+}
+
+export const WithError = Template.bind({})
+WithError.args = {
+  ...Primary.args,
+  error: 'This is an error',
+}
+
+export const WithHint = Template.bind({})
+WithHint.args = {
+  ...Primary.args,
+  hint: 'This is a hint',
+}
+
+export const WithResetOption = Template.bind({})
+WithResetOption.args = {
+  ...Primary.args,
+  shouldResetOption: true,
 }
