42b41188f6f32fd5f24b18aca8b4a7761d381d6f
vasileios.drosatos <vasileios.drosatos@workable.com>
2021-09-07 01:10:27 +0300

    Add localization on authentication

--- packages/pwa/next-i18next.config.js
+++ packages/pwa/next-i18next.config.js
@@ -1,9 +1,19 @@
 module.exports = {
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'el'],
+    defaultLocale: 'en-US',
+    locales: ['en-US', 'el'],
   },
   react: {
     useSuspense: false,
   },
+  domains: [
+    {
+      domain: 'forestrestoration.gr',
+      defaultLocale: 'el',
+    },
+    {
+      domain: 'forestrestore.app',
+      defaultLocale: 'en-US',
+    },
+  ],
 }
