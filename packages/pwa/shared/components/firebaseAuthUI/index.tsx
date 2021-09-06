92336aad0b951bff83d40299b57be1e75764c134
vasileios.drosatos <vasileios.drosatos@workable.com>
2021-09-07 01:56:33 +0300

    Add facebook provider

--- packages/pwa/shared/components/firebaseAuthUI/index.tsx
+++ packages/pwa/shared/components/firebaseAuthUI/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import firebase from 'firebase'
 
 const FIREBASEUI_CONTAINER_ID = 'firebaseui_container'
