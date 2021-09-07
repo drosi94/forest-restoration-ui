5edfad1638ea005903c2fdccc531fdbc65c72c5b
vasileios.drosatos <vasileios.drosatos@workable.com>
2021-09-07 12:58:57 +0300

    Revert "hotfix: Fix firebase analytics issue"
    This reverts commit d4e82a9e1f6236d769af8a3386b2c6f27553f2d5.

--- packages/pwa/firebase/clientApp.ts
+++ packages/pwa/firebase/clientApp.ts
@@ -12,22 +12,17 @@ export let firebaseApp;
 
 if (!firebase.apps.length) {
   firebaseApp = firebase.initializeApp(firebaseConfig);
-
-  // Enable analytics if env variable is set and the process is in browser
-  if(process.browser && process.env.NEXT_PUBLIC_USE_FIREBASE_ANALYTICS === 'true') {
-    require('firebase/analytics')
+  if(process.env.NEXT_PUBLIC_USE_FIREBASE_ANALYTICS === 'true') {
     firebase.analytics();
   }
 }
 
-// Use firebase emulators if the process is not in production and env variable is set
 if (process.env.NODE_ENV !== 'production' && process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR === 'true') {
   console.info('%c!!!!USING FIREBASE EMULATORS!!!!', "color:green; font-size: 26px")
   firebase.firestore().useEmulator("localhost", parseInt(process.env.NEXT_PUBLIC_FIREBASE_EMULATOR_FIRESTORE_PORT || '0'));
   firebase.functions().useEmulator("localhost", parseInt(process.env.NEXT_PUBLIC_FIREBASE_EMULATOR_FUNCTIONS_PORT || '0'));
 }
 
-// Use firebase authentication emulator if the process is not in production and env variable is set
 if (process.env.NODE_ENV !== 'production' && process.env.NEXT_PUBLIC_USE_FIREBASE_AUTH_EMULATOR === 'true') {
   console.info('%c!!!!USING FIREBASE AUTH EMULATOR!!!!', "color:green; font-size: 26px")
   firebase.auth().useEmulator(`http://localhost:${process.env.NEXT_PUBLIC_FIREBASE_EMULATOR_AUTH_PORT}`);
