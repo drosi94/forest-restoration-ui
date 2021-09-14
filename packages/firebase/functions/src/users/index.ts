0f206a0f71e567015ff4b7a22e811c825c6b002f
vasileios.drosatos <vasileios.drosatos@workable.com>
2021-09-14 04:09:18 +0300

    Send welcome email

--- packages/firebase/functions/src/users/index.ts
+++ packages/firebase/functions/src/users/index.ts
@@ -7,11 +7,30 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
-export const createUserDocument = functions.auth.user().onCreate((user: admin.auth.UserRecord) => {
-  console.info("Function Called: createUserDocument with data:", user);
+const sendWelcomeEmail = (userId: string, displayName = "") => {
+  return db.collection("mails").add({
+    toUids: [userId],
+    template: {
+      name: "welcome_el",
+      data: {
+        displayName
+      }
+    }
+  });
+};
 
-  return db.collection("users").doc(user.uid).set(transformUserAuthToUserDocument(user));
-});
+export const createUserDocument = functions.auth
+  .user()
+  .onCreate(async (user: admin.auth.UserRecord) => {
+    console.info("Function Called: createUserDocument with data:", user);
+    try {
+      await sendWelcomeEmail(user.uid, user.displayName);
+      return db.collection("users").doc(user.uid).set(transformUserAuthToUserDocument(user));
+    } catch (err) {
+      console.error("Function Error: createUserDocument with error:", err);
+      throw err;
+    }
+  });
 
 export const deleteUserDocument = functions.auth.user().onDelete((user: admin.auth.UserRecord) => {
   console.info("Function Called: deleteUserDocument with data:", user);
