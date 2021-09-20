3b7f489a76e296e6a0ceaa3b691fad67304f6192
vasileios.drosatos <vasileios.drosatos@workable.com>
2021-09-21 02:43:31 +0300

    Use animated button on home page

--- packages/pwa/pages/home/index.page.tsx
+++ packages/pwa/pages/home/index.page.tsx
@@ -3,7 +3,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
 import { toast } from 'react-hot-toast'
 import {
-  Button,
+  AnimatedButton as Button,
   Typography,
   Modal,
   Accordion,
