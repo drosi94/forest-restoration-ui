4cb953a0b1a99a265d9082abfe49f99059c2fd31
vasileios.drosatos <vasileios.drosatos@workable.com>
2021-09-21 01:47:24 +0300

    Update jest.config.js

--- packages/pwa/jest.config.js
+++ packages/pwa/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   roots: ['<rootDir>'],
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
-  testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
+  testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next|cypress)[/\\\\]'],
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
   transform: {
     '^.+\\.(ts|tsx)$': 'babel-jest',
