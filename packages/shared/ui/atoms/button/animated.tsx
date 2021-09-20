bb17a154d96d5c019b6c1ddc414876660f73fafa
vasileios.drosatos <vasileios.drosatos@workable.com>
2021-09-21 02:34:00 +0300

    Use framer motion to create animatable button

---
+++ packages/shared/ui/atoms/button/animated.tsx
@@ -0,0 +1,8 @@
+import { motion, MotionProps } from 'framer-motion'
+import { Button, ButtonProps } from '.'
+
+export const AnimatedButton: React.FC<ButtonProps & MotionProps> = ({
+  whileHover = { scale: 1.1 },
+  whileTap = { scale: 0.9 },
+  ...buttonProps
+}) => <Button as={motion.button} whileTap={whileTap} whileHover={whileHover} {...buttonProps} />
