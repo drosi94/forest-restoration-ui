04a8ea4dc2f7a023e797ea1d9fca416ec117085c
vasileios.drosatos <vasileios.drosatos@workable.com>
2021-09-21 02:38:42 +0300

    Update animated.tsx

--- packages/shared/ui/atoms/button/animated.tsx
+++ packages/shared/ui/atoms/button/animated.tsx
@@ -4,5 +4,14 @@ import { Button, ButtonProps } from '.'
 export const AnimatedButton: React.FC<ButtonProps & MotionProps> = ({
   whileHover = { scale: 1.1 },
   whileTap = { scale: 0.9 },
+  animate,
   ...buttonProps
-}) => <Button as={motion.button} whileTap={whileTap} whileHover={whileHover} {...buttonProps} />
+}) => (
+  <Button
+    as={motion.button}
+    whileTap={whileTap}
+    whileHover={whileHover}
+    animate={animate}
+    {...buttonProps}
+  />
+)
