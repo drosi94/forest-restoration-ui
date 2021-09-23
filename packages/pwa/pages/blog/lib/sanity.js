d0dcbf50ae0b84fe916b15b7fb59efd0a012cf80
vasileios.drosatos <vasileios.drosatos@workable.com>
2021-09-23 03:13:05 +0300

    Add blog components & sanity config

--- packages/pwa/pages/blog/lib/sanity.js
+++ packages/pwa/pages/blog/lib/sanity.js
@@ -1,49 +1,98 @@
+import React from 'react'
 import {
   createClient,
   createPortableTextComponent,
   createImageUrlBuilder,
   createPreviewSubscriptionHook
 } from "next-sanity";
+import BlockContent from "@sanity/block-content-to-react";
 
-import { config } from "./config";
+import { sanityConfig } from "./config";
+import { Typography } from '../../../../shared';
 
-if (!config.projectId) {
+if (!sanityConfig.projectId) {
   throw Error(
     "The Project ID is not set. Check your environment variables."
   );
 }
 export const urlFor = source =>
-  createImageUrlBuilder(config).image(source);
+  createImageUrlBuilder(sanityConfig).image(source);
 
 export const imageBuilder = source =>
-  createImageUrlBuilder(config).image(source);
+  createImageUrlBuilder(sanityConfig).image(source);
 
 export const usePreviewSubscription =
-  createPreviewSubscriptionHook(config);
+  createPreviewSubscriptionHook(sanityConfig);
 
-export const serializers = {
+  export const serializers = {
     types: {
-      code: (props) => (
-        <pre data-language={props.node.language}>
-          <code>{props.node.code}</code>
-        </pre>
-      ),
+      block: (props) => {
+        const { style = "normal" } = props.node;
+
+        if (style === 'normal') {
+          return React.createElement(
+            Typography,
+            { as: `p` , variant: 'body'},
+            props.children
+          );
+        }
+  
+        if (/^h\d/.test(style)) {
+          const level = style.replace(/[^\d]/g, "");
+          return React.createElement(
+            Typography,
+            { as: `h${level}` , variant: 'heading'},
+            props.children
+          );
+        }
+  
+        if (style === "blockquote") {
+          return <blockquote>- {props.children}</blockquote>;
+        }
+  
+        // Fall back to default handling
+        return BlockContent.defaultSerializers.types.block(props);
+      },
+      code: (props) =>
+         (
+          <pre data-language={props.node.language}>
+            <code>{props.node.code}</code>
+          </pre>
+        )
     },
-}
+    list: (props) =>
+      (props.type === "bullet" ? (
+        <ul>{props.children}</ul>
+      ) : (
+        <ol>{props.children}</ol>
+      )),
+    listItem: (props) =>
+      (props.type === "bullet" ? (
+        <li>{props.children}</li>
+      ) : (
+        <li>{props.children}</li>
+      )),
+    marks: {
+      strong: (props) =>
+         <strong>{props.children}</strong>,
+      em: (props) => <em>{props.children}</em>,
+      code: (props) =>  <code>{props.children}</code>
+    }
+  };
 
 // Set up Portable Text serialization
 export const PortableText = createPortableTextComponent({
-  ...config,
+  ...sanityConfig,
   // Serializers passed to @sanity/block-content-to-react
   // (https://github.com/sanity-io/block-content-to-react)
   serializers
 });
 
 
-export const client = createClient(config);
+export const client = createClient(sanityConfig);
 
 export const previewClient = createClient({
-  ...config,
+  ...sanityConfig,
   useCdn: false
 });
 
