281f7bc18f84c52e608af45f5c995ca64eb3b522
vasileios.drosatos <vasileios.drosatos@workable.com>
2021-09-24 23:30:36 +0300

    Fix navigation

--- packages/pwa/firebase/clientApp.ts
+++ packages/pwa/firebase/clientApp.ts
@@ -1,4 +1,4 @@
-import firebase from "firebase";
+import firebase from 'firebase'
 
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -7,35 +7,54 @@ export const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
-export let firebaseApp;
+}
+
+const firebaseApp = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app()
 
 if (!firebase.apps.length) {
-  firebaseApp = firebase.initializeApp(firebaseConfig);
-
   // Enable analytics if env variable is set and the process is in browser
-  if(process.browser && process.env.NEXT_PUBLIC_USE_FIREBASE_ANALYTICS === 'true') {
+  if (process.browser && process.env.NEXT_PUBLIC_USE_FIREBASE_ANALYTICS === 'true') {
     require('firebase/analytics')
-    firebase.analytics();
+    firebase.analytics()
   }
 
   // Enable app check if env variable is set and the process is in browser
   if (process.browser && process.env.NEXT_PUBLIC_RECAPTCHA_SITE_API_KEY) {
     require('firebase/app-check')
-    const appCheck = firebase.appCheck();
-    appCheck.activate(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_API_KEY, true);
+    const appCheck = firebase.appCheck()
+    appCheck.activate(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_API_KEY, true)
   }
 }
 
 // Use firebase emulators if the process is not in production and env variable is set
-if (process.env.NODE_ENV !== 'production' && process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR === 'true') {
-  console.info('%c!!!!USING FIREBASE EMULATORS!!!!', "color:green; font-size: 26px")
-  firebase.firestore().useEmulator("localhost", parseInt(process.env.NEXT_PUBLIC_FIREBASE_EMULATOR_FIRESTORE_PORT || '0'));
-  firebase.functions().useEmulator("localhost", parseInt(process.env.NEXT_PUBLIC_FIREBASE_EMULATOR_FUNCTIONS_PORT || '0'));
+if (
+  process.env.NODE_ENV !== 'production' &&
+  process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR === 'true'
+) {
+  console.info('%c!!!!USING FIREBASE EMULATORS!!!!', 'color:green; font-size: 26px')
+  firebase
+    .firestore()
+    .useEmulator(
+      'localhost',
+      parseInt(process.env.NEXT_PUBLIC_FIREBASE_EMULATOR_FIRESTORE_PORT || '0')
+    )
+  firebase
+    .functions()
+    .useEmulator(
+      'localhost',
+      parseInt(process.env.NEXT_PUBLIC_FIREBASE_EMULATOR_FUNCTIONS_PORT || '0')
+    )
 }
 
 // Use firebase authentication emulator if the process is not in production and env variable is set
-if (process.env.NODE_ENV !== 'production' && process.env.NEXT_PUBLIC_USE_FIREBASE_AUTH_EMULATOR === 'true') {
-  console.info('%c!!!!USING FIREBASE AUTH EMULATOR!!!!', "color:green; font-size: 26px")
-  firebase.auth().useEmulator(`http://localhost:${process.env.NEXT_PUBLIC_FIREBASE_EMULATOR_AUTH_PORT}`);
+if (
+  process.env.NODE_ENV !== 'production' &&
+  process.env.NEXT_PUBLIC_USE_FIREBASE_AUTH_EMULATOR === 'true'
+) {
+  console.info('%c!!!!USING FIREBASE AUTH EMULATOR!!!!', 'color:green; font-size: 26px')
+  firebase
+    .auth()
+    .useEmulator(`http://localhost:${process.env.NEXT_PUBLIC_FIREBASE_EMULATOR_AUTH_PORT}`)
 }
+
+export { firebaseApp, firebase }
--- packages/pwa/pages/_app.page.tsx
+++ packages/pwa/pages/_app.page.tsx
@@ -1,30 +1,20 @@
 import Head from 'next/head'
-import Link from 'next/link'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { appWithTranslation } from 'next-i18next'
 import { GlobalStyles } from 'twin.macro'
-import {
-  Button,
-  Logo,
-  Navbar,
-  Toast,
-  Typography,
-  UserActionsPopover,
-} from '@forest-restoration/shared'
-
+import { Toast } from '@forest-restoration/shared'
 import { useFixHeightViewport } from '../shared/hooks/useFixHeightViewport'
 import { ThemeProvider, useThemeProvider } from '../shared/providers/themeProvider'
+import { Navigation } from '../shared/components/navigation'
 import '../theme.css'
 import '../firebase/clientApp'
-import logoImage from '../public/icon-384x384.png'
-import { ToggleTheme } from '../shared/components/toggleTheme'
 
 function MyApp({ Component, pageProps }) {
   const { isDarkMode } = useThemeProvider()
   useFixHeightViewport()
   const { route } = useRouter()
-  const isAuthenticated = false
 
   return (
     <>
@@ -34,50 +24,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <GlobalStyles />
-      {route !== '/maintenance' && (
-        <header>
-          <Navbar
-            customLogo={
-              <Logo
-                CustomImageComponent={<Image alt="Logo" src={logoImage} width="50" height="50" />}
-              />
-            }
-            primaryNav={
-              <>
-                <Link href="/home" passHref>
-                  <a>
-                    <Typography tw="hover:opacity-70">Home</Typography>
-                  </a>
-                </Link>
-                <Link href="/blog" passHref>
-                  <a>
-                    <Typography tw="hover:opacity-70">Blog</Typography>
-                  </a>
-                </Link>
-              </>
-            }
-            secondaryNav={
-              <>
-                {isAuthenticated ? (
-                  <UserActionsPopover displayName="test">
-                    <div tw="flex flex-col">
-                      <a href="#" tw="py-3 px-2 rounded hover:bg-primary-500">
-                        <Typography tw="hover:opacity-70">Logout</Typography>
-                      </a>
-                      <a href="#" tw="py-3 px-2 rounded hover:bg-primary-500">
-                        <Typography tw="hover:opacity-70">Logout</Typography>
-                      </a>
-                    </div>
-                  </UserActionsPopover>
-                ) : (
-                  <Button variant="text">Login</Button>
-                )}
-                <ToggleTheme />
-              </>
-            }
-          />
-        </header>
-      )}
+      {route !== '/maintenance' && <Navigation />}
       <div tw="flex flex-col h-[calc(100 * var(--vh))]">
         <main tw="flex-1">
           <Component {...pageProps} />
