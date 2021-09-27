f97fb828ae6c5443026e78595c545d366fb34831
vasileios.drosatos <vasileios.drosatos@workable.com>
2021-09-27 23:53:57 +0300

    Navigation tweaks

--- packages/pwa/shared/components/navigation/navLink/index.tsx
+++ packages/pwa/shared/components/navigation/navLink/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import tw, { css } from 'twin.macro'
+import tw from 'twin.macro'
 import Link, { LinkProps } from 'next/link'
 
 import { Typography } from '../../../../../shared'
@@ -8,9 +8,10 @@ import { useRouter } from 'next/router'
 type NavLinkProps = LinkProps & {
   title: string
   exact?: boolean
+  icon?: React.ReactNode
 }
 
-export const NavLink: React.FC<NavLinkProps> = ({ title, href, exact, ...linkProps }) => {
+export const NavLink: React.FC<NavLinkProps> = ({ title, href, exact, icon, ...linkProps }) => {
   const { asPath } = useRouter()
 
   const hrefString = typeof href === 'string' ? href : href.href
@@ -18,12 +19,20 @@ export const NavLink: React.FC<NavLinkProps> = ({ title, href, exact, ...linkPro
   const isActive = exact ? asPath === hrefString : asPath.includes(hrefString)
 
   return (
-    <Link href={href} {...linkProps}>
-      <a>
-        <Typography css={[tw`hover:opacity-70`, isActive && tw`text-primary-500`]}>
-          {title}
-        </Typography>
-      </a>
-    </Link>
+    <li>
+      <Link href={href} passHref {...linkProps}>
+        <a
+          css={[
+            tw`flex gap-2 uppercase text-textPrimary hover:opacity-70`,
+            isActive && tw`text-primary-500`,
+          ]}
+        >
+          {icon}
+          <Typography fontFamily="notoSerif" tw="text-current">
+            {title}
+          </Typography>
+        </a>
+      </Link>
+    </li>
   )
 }
