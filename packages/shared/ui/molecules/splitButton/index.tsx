1ebeb50460da99011d0bf34b3fa278b3ead3bcd9
vasileios.drosatos <vasileios.drosatos@workable.com>
2021-10-06 22:45:34 +0300

    Add actions

--- packages/shared/ui/molecules/splitButton/index.tsx
+++ packages/shared/ui/molecules/splitButton/index.tsx
@@ -1,35 +1,80 @@
 import React from 'react'
 import tw from 'twin.macro'
-import { Typography } from '../..'
 import { Button } from '../../atoms/button'
 import { Popover } from '../popover'
-import { ChevronUpIcon } from '@heroicons/react/solid'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+import { Popover as BasePopover } from '@headlessui/react'
+import { Typography } from '../..'
+
+export type Action = {
+  id: string
+  label: string
+  onClick: () => void
+}
 
 export type SplitButtonPopoverProps = {
-  label: string
+  /**
+   * The actions to display in the popover
+   */
+  actions: Action[]
+  /**
+   * The selected action id to display in the button. If it is not defined the first action will be selected
+   */
+  selectedActionId?: string
 }
 
 export const SplitButtonPopover: React.FC<SplitButtonPopoverProps> = ({
-  label,
-  children,
+  actions,
+  selectedActionId,
   ...rest
 }) => {
+  if (selectedActionId && actions.find((action) => action.id === selectedActionId) === undefined) {
+    throw new Error(`The selected action id ${selectedActionId} is not defined in the actions`)
+  }
+
+  const [selectedAction, setSelectedAction] = React.useState<Action>(
+    selectedActionId ? actions.find((action) => action.id === selectedActionId) : actions[0]
+  )
+
+  const { label, onClick } = selectedAction
+
   const containerButton = React.forwardRef<any, any>((props, ref) => {
     return (
-      <Button ref={ref} aria-label={label} {...props} {...rest} tw="!rounded-l-none !rounded-r-md !flex items-center justify-center ">
-          <ChevronUpIcon css={[tw` w-5 h-5`]} />
+      <Button
+        ref={ref}
+        aria-label={label}
+        {...props}
+        tw="!rounded-l-none !rounded-r-md !flex items-center justify-center "
+      >
+        <ChevronDownIcon css={[tw` w-5 h-5`]} />
       </Button>
     )
   })
+
+  const handleActionClick = (action: Action) => {
+    setSelectedAction(action)
+  }
+
   return (
     <div tw="flex">
-      <Button tw="!rounded-r-none !rounded-l-md">lol</Button>
+      <Button tw="!rounded-r-none !rounded-l-md" onClick={onClick} {...rest}>
+        {label}
+      </Button>
       <Popover
         label={label}
         buttonComponent={containerButton}
-        overrideButtonStyles={tw`p-0! block h-10 w-10 rounded-full overflow-hidden `}
+        overrideButtonStyles={tw`p-0! block h-10 w-10 rounded-full overflow-hidden border-0 border-l-2 border-secondary`}
       >
-        <div tw="bg-bgSecondary text-textPrimary border border-gray-400">{children}</div>
+        <div tw="flex flex-col w-48 md:w-72 bg-base-200">
+          {actions.map((action) => (
+            <BasePopover.Button
+              onClick={() => handleActionClick(action)}
+              tw="py-3 px-2 text-left hover:bg-primaryTemp-500"
+            >
+              <Typography>{action.label}</Typography>
+            </BasePopover.Button>
+          ))}
+        </div>
       </Popover>
     </div>
   )
--- packages/ui/stories/ui/SplitButton.stories.tsx
+++ packages/ui/stories/ui/SplitButton.stories.tsx
@@ -22,5 +22,20 @@ const otherButtons = (
 export const Primary = Template.bind({})
 Primary.args = {
   displayName: 'Test user',
-  children: otherButtons,
+  actions: [
+    {
+      id: 'Action 1',
+      label: 'Action 1',
+      onClick: () => {
+        console.log('action1')
+      },
+    },
+    {
+      id: 'Action 2',
+      label: 'Action 2',
+      onClick: () => {
+        console.log('action2')
+      },
+    },
+  ],
 }
